fc0a010011fc617038490c963ec052f05aeb6bb1
WillianPer <wsresilienttasteshopw@gmail.com>
2022-07-27 15:51:07 -0400

    Criação do Layout e titulo usando Next.js e TailwindCSS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
+import Layout from "../components/Layout";
+
 export default function Home() {
   return (
-
-    <div className={` `}/*"flex"*/>
-      
+    <div className={`flex justify-center items-center h-screen
+    bg-gradient-to-r to-blue-500 from-purple-500
+    text-white`}/*"flex"*/>
+      <Layout titulo="Cadastro Simples">
+        <span>Conteudo</span>
+      </Layout>
     </div>
+
+    // <h1 className="text-3xl font-bold underline">
+    //   Hello world!
+    // </h1>
   )
 
   // return (
