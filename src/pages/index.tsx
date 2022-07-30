d99e67877a223d09e7df4bf1b514ebefd4acd879
WillianPer <wsresilienttasteshopw@gmail.com>
2022-07-29 22:53:00 -0400

    Criação do Componente Tabela!!

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,22 @@
 import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from "../core/Cliente";
 
 export default function Home() {
+
+  const clientes = [
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Bia', 45, '2'),
+    new Cliente('Carlos', 23, '3'),
+    new Cliente('Pedro', 54, '4')
+  ]
+
   return (
     <div className={`flex justify-center items-center h-screen
     bg-gradient-to-r to-blue-500 from-purple-500
     text-white`}/*"flex"*/>
       <Layout titulo="Cadastro Simples">
-        <span>Conteudo</span>
+        <Tabela clientes={clientes}></Tabela>
       </Layout>
     </div>
 
