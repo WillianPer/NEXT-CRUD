0e55750586a10074f6e094632d9a7394a3f5ec55
WillianPer <wsresilienttasteshopw@gmail.com>
2022-08-05 00:03:46 -0400

    Alternando entre Tabela Formulário!!

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,8 @@ import Tabela from "../components/Tabela";
 import Cliente from "../core/Cliente";
 
 export default function Home() {
+  const [cliente, setCliente] = useState<Cliente>(Cliente.vazio())
+  const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
 
   const clientes = [
     new Cliente('Ana', 34, '1'),
@@ -15,18 +17,24 @@ export default function Home() {
   ]
 
   function clienteSelecionado( cliente: Cliente) {
-    console.log(cliente.nome)
+    setCliente(cliente)
+    setVisivel('form')
   }
 
   function clienteExcluido( cliente: Cliente) {
     console.log(`Excluir... ${cliente.nome}`)
   }
 
-  function salvarCliente(cliente: Cliente) {
-    console.log(cliente)
+  function novoCliente() {
+    setCliente(Cliente.vazio())
+    setVisivel('form')
   }
 
-  const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
+  function salvarCliente(cliente: Cliente) {
+    console.log(cliente)
+    setVisivel('tabela')
+  }
+
 
   return (
     <div className={`flex justify-center items-center h-screen
@@ -42,7 +50,7 @@ export default function Home() {
             <div className="flex justify-end">
 
               <Botao cor="green" className="mb-4"
-                onClick={() => setVisivel('form')}>
+                onClick={novoCliente}>
                 Novo Cliente
               </Botao>
 
@@ -57,7 +65,7 @@ export default function Home() {
         ) : (
           
           <Formulario 
-            cliente={clientes[0]}
+            cliente={cliente}
             clienteMudou={salvarCliente}
             cancelado={() => setVisivel('tabela')}/>
 
